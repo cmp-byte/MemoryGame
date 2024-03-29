5befaccc279a96859912d8bcf369e14f64fb62d6
cmp-byte <razvancampian1999@yahoo.com>
2024-03-29 16:18:34 +0200

    Initial commit

---
+++ MemoryGame/MemoryGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemoryGameApp.swift
+//  MemoryGame
+//
+//  Created by Student on 29.03.2024.
+//
+
+import SwiftUI
+
+@main
+struct MemoryGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
